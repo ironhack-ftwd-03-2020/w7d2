6a6d0ac45be6e1c30e3fa9099a4c046b6ffc496e
Jan Redmann <janredmann1@gmail.com>
2020-05-05 15:51:46 +0200

    add list-and-conditionals

--- list-and-keys/src/App.js
+++ list-and-keys/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MoviesList from './MoviesList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const moviesData = [
+  { 'hasOscars': false, 'title': 'The Shawshank Redemption', 'director': 'Frank Darabont', 'rate': '9.3', 'id': 0 },
+  { 'hasOscars': true, 'title': 'The Godfather', 'director': 'Francis Ford Coppola', 'rate': '9.2', 'id': 1 },
+  { 'hasOscars': true, 'title': 'The Godfather: Part II', 'director': 'Francis Ford Coppola', 'rate': '9.0', 'id': 2 },
+  { 'hasOscars': true, 'title': 'The Dark Knight', 'director': 'Christopher Nolan', 'rate': '9.0', 'id': 3 },
+  { 'hasOscars': false, 'title': '12 Angry Men', 'director': 'Sidney Lumet', 'rate': '8.9', 'id': 4 }
+];
+
+class App extends Component {
+
+  state = {
+    movies: moviesData
+  }
+
+  addMovie = () => {
+    const newMovie = { 'hasOscars': true, 'title': 'Interstellar', 'director': 'Christopher Nolan', 'rate': '8.6', 'id': 31 };
+    // const moviesCopy = this.state.movies.slice();
+    // moviesCopy.push(newMovie);
+    this.setState({
+      // movies: moviesCopy
+      movies: this.state.movies.concat(newMovie)
+    })
+  }
+
+  render() {
+    // const movieListItems = this.state.movies.map((movie, idx) => (<li key={movie.id} >{movie.title}</li>));
+
+    return (
+      <div className='App'>
+        <h1>Movies</h1>
+        {/* <ul>{movieListItems}</ul> */}
+        <button onClick={this.addMovie}>Add a movie</button>
+        {this.state.movies.length === 0 && <h2> No movies here</h2>}
+
+        <MoviesList movies={this.state.movies} />
+      </div >
+    );
+  }
 }
 
 export default App;
---
+++ list-and-keys/src/MoviesList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+export default function MoviesList(props) {
+
+    const movies = props.movies.map(movie => {
+        return (
+            <div key={movie.id}>
+                <h2>{movie.title}</h2>
+                <p>{movie.director}</p>
+                <p>{movie.rate}</p>
+                {movie.hasOscars ? <p>Oscar winning movie 🤩</p> : <p>No Oscar</p>}
+            </div>
+        )
+    })
+
+    return <div>{movies}</div>
+
+}
